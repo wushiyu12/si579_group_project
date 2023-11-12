b1fcad0576e4ff1180f1f1b0eb48c878085b3e27
wushiyu12 <116837748+wushiyu12@users.noreply.github.com>
2023-11-12 16:48:07 +0000

    Create some sample courses for the UI

--- si579proj/src/util/course.js
+++ si579proj/src/util/course.js
@@ -1,1 +1,29 @@
-export const courseList = []
+export const courseList = [
+    {
+        'subject': 'sample',
+        'catalog #': 101,
+        'course title': 'sample, ample, and mple',
+        'current enforced prerequisites': null,
+        'current advisory prerequisites': null,
+        'course description': 'Introduction to sample. We will be using asynchronous functions to retrieve the course information.',
+        'credits': 4
+    },
+    {
+        'subject': 'sample',
+        'catalog #': 106,
+        'course title': 'more samples',
+        'current enforced prerequisites': null,
+        'current advisory prerequisites': 'sample 101',
+        'course description': 'For now, we will be using sample data',
+        'credits': 4
+    },
+    {
+        'subject': 'sample',
+        'catalog #': 200,
+        'course title': 'still samples',
+        'current enforced prerequisites': 'sample 101, sample 106',
+        'current advisory prerequisites': null,
+        'course description': 'Advanced samples.',
+        'credits': 4
+    },
+]
