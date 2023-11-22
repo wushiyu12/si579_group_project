b4673284f71b1eaf1a21e79c8b49f22636569fa7
Zixi Zhou <112566694+Zixiiii@users.noreply.github.com>
2023-11-22 17:52:49 +0000

    Beautify ListView Cards Layout

--- si579proj/src/component/CourseCard.js
+++ si579proj/src/component/CourseCard.js
@@ -12,12 +12,16 @@ const CourseCard = (props) => {
     return (
         <Card style={{ width: '24rem'}}>
           <Card.Img variant="top" src="https://yt3.googleusercontent.com/ytc/APkrFKY7Ofoz3DRteo2LvDggtO1FAUcsEa-sGL64SM9aDQ=s900-c-k-c0x00ffffff-no-rj" />
-          <Card.Body>
-            <Card.Title>{props.code}</Card.Title>
-            <Card.Text>
-              {props.title}
-            </Card.Text>
-            <Button variant="primary" onClick={handleShow}>See Course Description</Button>
+          <Card.Body style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+            <div>
+              <Card.Title>{props.code}</Card.Title>
+              <Card.Text>
+                {props.title}
+              </Card.Text>
+            </div>
+            <div style={{marginTop: '12px'}}>
+              <Button variant="primary" onClick={handleShow}>See Course Description</Button>
+            </div>
             <Offcanvas show={show} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>{props.title}</Offcanvas.Title>
--- si579proj/src/component/CourseCardArea.js
+++ si579proj/src/component/CourseCardArea.js
@@ -1,11 +1,32 @@
 import {courseList} from '../util/course';
 import CourseCard from './CourseCard';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import CardGroup from 'react-bootstrap/CardGroup';
+import { useState } from 'react';
+
+// const CourseCardArea = () => {
+//     return (
+//         <Container>
+//             <Row xs={1} md={2} lg={3}>
+//             {courseList.map((course) => <Col><CourseCard code={course['code']} title={course['Course Title']} description={course['Course Description']} /></Col>)}
+//             </Row>
+//         </Container>
+//     )
+// };
 
 const CourseCardArea = () => {
+    const [courses, setCourses] = useState(courseList);
+    const arr = courses.reduce((item, key, index) => (index % 3 == 0 ? item.push([key]) : item[item.length-1].push(key)) && item, []);
     return (
-        <>
-            {courseList.map((course) => <CourseCard code={course['code']} title={course['Course Title']} description={course['Course Description']} />)}
-        </>
+        <Container>
+            {arr.map(group => 
+                <row><CardGroup>
+                    {group.map(course => <CourseCard code={course['code']} title={course['Course Title']} description={course['Course Description']} />)}
+                </CardGroup></row>
+            )}
+        </Container>
     )
 };
 export default CourseCardArea
