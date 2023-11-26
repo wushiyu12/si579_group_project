ad0e15f5e72c057720c14fa74a34d910a885b9a4
Zixi Zhou <112566694+Zixiiii@users.noreply.github.com>
2023-11-26 02:01:32 +0000

    style2

--- si579proj/src/pages/CalendarView.js
+++ si579proj/src/pages/CalendarView.js
@@ -4,21 +4,25 @@ import Container from 'react-bootstrap/Container';
 import { useEffect, useState } from 'react';
 import SearchBar from '../component/SearchBar';
 import CourseProfile from '../component/CourseProfile';
-
+import HomePageSearchBox from '../component/HomePageSearchBox';
 
 const CalendarView = () => {
     const [chooseRes, setChooseRes] = useState(null);
 
     return (
-        <>
-            <h2>This is the calendar view</h2>
+        <div style={{backgroundColor:'#f8f9fa'}}>
+            <div className="text-center  py-3" style={{height:'100px',backgroundColor: '#1565c0'}}>
+                <h2 style={{fontSize:30, fontWeight:'bold', paddingTop:'2%', color:'white'}}>This is the calendar view</h2>
+            </div>
             <NavBar />
-            <Container>
+
+            {/* <Container>
             <SearchBar setChooseRes = {setChooseRes} renderDropDowm = {true}></SearchBar>
             {chooseRes && <CourseProfile chooseRes = {chooseRes}></CourseProfile>}
-            </Container>
+            </Container> */}
+            <HomePageSearchBox />
             <Calendar />
-        </>
+        </div>
     )
 }
 
