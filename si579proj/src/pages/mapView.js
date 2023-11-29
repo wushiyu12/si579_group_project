4ae6c5ea1673729e7c2e533e1aa9016a8600a899
Zixi Zhou <112566694+Zixiiii@users.noreply.github.com>
2023-11-29 16:31:55 +0000

    map

--- si579proj/src/pages/mapView.js
+++ si579proj/src/pages/mapView.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import NavBar from '../component/NavBar';
+import { MapContainer, TileLayer, Marker, useMap , Popup} from 'react-leaflet'
+
 
 const mapView = () => {
     return (
@@ -8,7 +10,20 @@ const mapView = () => {
             <h2 style={{fontSize:30, fontWeight:'bold', paddingTop:'2%', color:'white'}}>This is the map view</h2>
           </div>
             {/* <h2>This is the map view</h2> */}
-            <NavBar />
+          <NavBar />
+          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+
+          
         </div>
     )
 }
