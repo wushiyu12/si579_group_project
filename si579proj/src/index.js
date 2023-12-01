66f4a625511eef49586afe05bcf2a5ab3df82eb3
jkchi <113892417+jkchi@users.noreply.github.com>
2023-12-01 22:21:43 +0000

    remove not needed file

--- si579proj/src/index.js
+++ si579proj/src/index.js
@@ -15,18 +15,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render((
   <BrowserRouter>
     <App /> 
-    {/* The various pages will be displayed by the `Main` component. */}
-    {/* but do we reall need the main? */}
+
   </BrowserRouter>
   )
 );
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
